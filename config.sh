de99802d38e5c3161c6d6288c39397166a7b232e
Amirali Dashti <devoft1@gmail.com>
2025-05-17 20:00:02 +0330

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,14 @@
+arch-chroot /mnt
+
+pacman -Syu git --no-confirm
+
+cd /tmp/
+
+git clone https://www.github.com/maibloom/omnipkg-app
+
+cd omnipkg-app/
+
+if sudo bash build.sh; then
+  omnipkg put install maibloom-builder
+else
+  echo "running build has failed"
