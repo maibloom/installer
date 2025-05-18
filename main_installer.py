6915cacd75182b4a0b0392c72d9171a54d45341e
Amirali Dashti <devoft1@gmail.com>
2025-05-18 09:46:10 +0330

    Update main_installer.py

--- main_installer.py
+++ main_installer.py
@@ -4,8 +4,34 @@ import subprocess
 import time
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton, QMessageBox
 from PyQt5.QtGui import QFont
-from installation-pipe import installprocess
 
+def installpipe():
+  archinstall_konsole_command = "archinstall; exec bash"
+  archinstall_process = subprocess.Popen(["konsole", "-e", "bash", "-c", archinstall_konsole_command])
+        
+  return_code = archinstall_process.wait() 
+
+  if return_code == 0:
+    print("Archinstall konsole window closed. Assuming successful completion by user. Proceeding with post-installation steps...")
+            
+    post_install_chroot_script = (
+      "pacman -Syu --noconfirm git && "
+      "echo 'Git installed/updated.' && "
+      "rm -rf /installer && "
+      "echo 'Removed old /installer directory if it existed.' && "
+      "git clone https://github.com/maibloom/installer /installer && "
+      "echo 'Cloned maibloom/installer to /installer.' && "
+      "cd /installer && "
+      "chmod +x config.sh && "
+      "echo 'Made config.sh executable.' && "
+      "./config.sh && " 
+      "echo 'Post-installation script (config.sh) finished. You can close this terminal.' && "
+      "exec bash"
+      )
+            
+      post_install_command = f"arch-chroot /mnt /bin/bash -c '{post_install_chroot_script}'"
+            
+      post_install_process = subprocess.Popen(post_install_command, shell=True)
 
 class InstallerWindow(QWidget):
     def __init__(self):
@@ -43,7 +69,7 @@ class InstallerWindow(QWidget):
         
         time.sleep(2)
 
-        installprocess()
+        installpipe()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
